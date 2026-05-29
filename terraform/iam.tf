de7c098002b308b83c677270334d076a21eebd7b
SushrutaEswar <141320972+SushrutaEswar@users.noreply.github.com>
2026-05-29 16:20:34 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,7 @@
+############################################
+# EC2 ROLE (for your instances)
+############################################
+
 resource "aws_iam_role" "ec2_role" {
   name = "dotnet-ec2-role"
 
@@ -18,9 +22,15 @@ resource "aws_iam_role_policy_attachment" "ec2_ssm" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_role_policy_attachment" "ec2_s3" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+# REQUIRED for CodeDeploy agent on EC2
 resource "aws_iam_role_policy_attachment" "ec2_codedeploy" {
   role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeployLimited"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
@@ -28,68 +38,87 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   role = aws_iam_role.ec2_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "ec2_s3" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+############################################
+# CODEDEPLOY ROLE (FIXED - IMPORTANT)
+############################################
+
+resource "aws_iam_role" "codedeploy_role" {
+  name = "codedeploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "codedeploy.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
+
+# REQUIRED: Core CodeDeploy permissions
+resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
+  role       = aws_iam_role.codedeploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+}
+
+# REQUIRED FIX: Auto Scaling permissions (THIS FIXES YOUR ERROR)
+resource "aws_iam_role_policy_attachment" "codedeploy_asg" {
+  role       = aws_iam_role.codedeploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AutoScalingFullAccess"
+}
+
+############################################
+# CODEBUILD ROLE
+############################################
 
 resource "aws_iam_role" "codebuild_role" {
   name = "dotnet-codebuild-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-
-        Principal = {
-          Service = "codebuild.amazonaws.com"
-        }
-
-        Action = "sts:AssumeRole"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "codebuild.amazonaws.com"
       }
-    ]
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "codebuild_logs" {
-
   role       = aws_iam_role.codebuild_role.name
-
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "codebuild_s3" {
-
   role       = aws_iam_role.codebuild_role.name
-
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "codebuild_codepipeline" {
-
+resource "aws_iam_role_policy_attachment" "codebuild_dev" {
   role       = aws_iam_role.codebuild_role.name
-
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
 }
+
+############################################
+# CODEPIPELINE ROLE
+############################################
 
 resource "aws_iam_role" "pipeline_role" {
   name = "dotnet-pipeline-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-
-    Statement = [
-      {
-        Effect = "Allow"
-
-        Principal = {
-          Service = "codepipeline.amazonaws.com"
-        }
-
-        Action = "sts:AssumeRole"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
       }
-    ]
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
