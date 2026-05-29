e08b8650ded7a50227843d815fd6145298b7c884
SushrutaEswar <141320972+SushrutaEswar@users.noreply.github.com>
2026-05-29 17:21:37 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -19,3 +19,14 @@ resource "aws_subnet" "public2" {
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
 }
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public1_assoc" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public_rt.id
+}
